30e6a7f2d501445cf755ad591e21bfd7ef77e363
Marko Översti <marko.oversti@gmail.com>
2016-02-18 14:49:03 +0200

    sketch script

--- script.js
+++ script.js
@@ -1,4 +1,30 @@
 $(document).ready(function(){
-	$('body').prepend('<div class="container"></div>')
-	
+	newGrid();
 });
+
+function newGrid() {
+	var input = prompt('Type an integer between 10 and 100');
+	$('#grid').empty();
+	if (input >= 10 && input <= 100) {
+		var cube_size = $('#grid').width()/input - 2;
+		for(var i = 1; i <= input; i++) {
+			for(var j = 1; j <= input; j++) {
+				$('#grid').append('<div class="cube"></div>');
+			}
+			$('#grid').append('<div class="row"></div>');
+		}
+		$('.cube').css('width',cube_size);
+		$('.cube').css('height',cube_size);
+		
+		
+		$('.cube').mouseenter(function(){
+			var opacity = $(this).css('opacity');
+			if (opacity > 0) {
+				$(this).css('opacity', opacity - 0.2);
+			}
+		});
+	} else {
+		alert('Sorry, not an appropriate number');
+	}
+	
+}
